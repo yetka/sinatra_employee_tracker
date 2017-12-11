e001984dcbae69a86ee551c34980f5393e2e1b01
Malgorzata Haniszewska <gosia.haniszewska@gmail.com>
2017-12-11 13:28:15 -0800

    fix specs

--- spec/division_spec.rb
+++ spec/division_spec.rb
@@ -1,11 +1,12 @@
 require("spec_helper")
 
 describe(Division) do
-  describe("#division") do
-    it("tells which division it belongs to") do
-      test_division = Division.create({:name => "HR"})
-      test_employee = Employee.create({:name => "Abraham Lincoln", :division_id => test_division.id})
-      expect(test_employee.division()).to(eq(test_division))
+  describe("#employees") do
+    it("tells which employees are in it") do
+      test_division = Division.create({:name => "division"})
+      test_employee1 = Employee.create({:name => "Abraham Lincoln", :division_id => test_division.id})
+      test_employee2 = Employee.create({:name => "John Doe", :division_id => test_division.id})
+     expect(test_division.employees()).to(eq([test_employee1, test_employee2]))
     end
   end
 end
